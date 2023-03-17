a779edc829f205513234991927d7529288814885
Victor Carvalho <vimatheuscar0@gmail.com>
2023-03-17 18:20:24 -0300

    Update formulario.js

--- js/formulario.js
+++ js/formulario.js
@@ -6,7 +6,6 @@ function formularioEnviado(resposta) {
   }else {
     formulario.innerHTML = "<p class='font-2-l' style='grid-column: 1/-1; padding: 1rem; border-radius: 4px; background: #f7f7f7;'><span style='color: #E00000;'>Erro no envio!</span> Você pode enviar diretamente para o nosso email em: contato@afetoempontos.com</p>"
   }
-  "gabriel"
 }
 
 function enviarFormulario(event) {
